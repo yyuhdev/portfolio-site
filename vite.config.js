a2e02bda187c6d485484f2de4c61eb357acf3cda
yyuh <dustinohlemann@gmail.com>
2026-01-10 17:23:05 +0100

    duh

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio-site/',
   plugins: [
-      tailwindcss(),
+    tailwindcss(),
     vue(),
     vueDevTools(),
   ],
